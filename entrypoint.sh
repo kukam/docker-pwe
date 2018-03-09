a4127b201c795fa3b94958425b5e31fdf258d0c0
kukam <dusan.grmela@gmail.com>
2018-03-09 05:34:57 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,9 @@
 #!/bin/sh
 
-if [ -d "/PWE/.svn" ]; then
-    svn up /PWE
-else
-    svn co $PWESOURCE /PWE
-fi
-
-if [ -d "/PWE/webapps/myproject/.svn" ]; then
+if [ -d .svn ]; then
     svn up ./
 else
-    svn co $MYPROJECT /PWE/webapps/myproject
+    svn co $MYPROJECT ./
 fi
-
-cd /PWE/webapps/myproject
-
-export PWE_CONF_pwe_home="/PWE/webapps/myproject"
 
 exec "$@"
